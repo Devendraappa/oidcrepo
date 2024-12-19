5bbca34df27df1ad68460a29922125ca51681dbc
Devendraappa <devendraappa.kune@gmail.com>
2024-12-19 19:45:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_count" {
+  default = 2
+}
+
+variable "private_subnet_count" {
+  default = 2
+}
+
+variable "availability_zones" {
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "ami_id" {
+  default = "ami-0c02fb55956c7d316"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ec2_instance_count" {
+  default = 2
+}
+
+variable "key_name" {
+  default = "your-key-pair-name"
+}
+
+variable "project_name" {
+  default = "my-project"
+}
